7660321ecde02f58450079a5b0baef5a1eb98024
Enl1ghtener <yumuc000@gmail.com>
2024-02-14 22:43:16 +0800

    变更

--- crwal/crwal_houseprice.py
+++ crwal/crwal_houseprice.py
@@ -1,6 +1,5 @@
 import requests
 import re
-
 def get_price(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/'
                              '537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'}
